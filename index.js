91735264367b8e699ed14bd352c8d10a09084594
Fabio Squizzato <fsquizzato@visualmedica.com>
2023-04-05 17:59:30 -0300

    funcionalidades vue-->error blind clase

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 new Vue({
-    el: '#app',
-    listasTotales: 10
-})
+  el: "#app",
+  data: {
+    listasTotalesPaso: [
+      { nombre: "1A", clase: "uno" },
+      { nombre: "1B", clase: "uno" },
+      { nombre: "1C", clase: "uno" },
+      { nombre: "2A", clase: "dos" },
+      { nombre: "2B", clase: "dos" },
+      { nombre: "3A", clase: "tres" },
+      { nombre: "4A", clase: "cuatro" },
+      { nombre: "4B", clase: "cuatro" },
+      { nombre: "4C", clase: "cuatro" },
+      { nombre: "4D", clase: "cuatro" },
+    ],
+    listasTotales: ["1", "2", "3", "4", "5"],
+  },
+});
