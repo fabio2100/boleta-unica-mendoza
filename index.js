3c597524ede3d43ac6b40e0fecd4abd91f5e23ad
Fabio Squizzato <fsquizzato@visualmedica.com>
2023-04-18 17:53:47 -0300

    select tipo eleccion

--- index.js
+++ index.js
@@ -1,45 +1,61 @@
 Vue.component("modal", {
   template: "#modal-template",
-  props : ['votoGobernador','votoSenadores','votoDiputados','votoIntendente','votoConcejales']
+  props: [
+    "votoGobernador",
+    "votoSenadores",
+    "votoDiputados",
+    "votoIntendente",
+    "votoConcejales",
+  ],
 });
 
 new Vue({
   el: "#app",
   data: {
-    showModal : false,
-    checkedValuesListaCompleta : [],
+    tipoEleccion: 'paso',
+    showModal: false,
+    checkedValuesListaCompleta: [],
     checkedValuesGobernador: [],
     checkedValuesSenadores: [],
     checkedValuesDiputados: [],
     checkedValuesIntendente: [],
     checkedValuesConcejales: [],
-    votoGobernador : 0,
-    votoSenadores : 0,
-    votoDiputados : 0,
-    votoIntendente : 0,
-    votoConcejales : 0,
+    votoGobernador: 0,
+    votoSenadores: 0,
+    votoDiputados: 0,
+    votoIntendente: 0,
+    votoConcejales: 0,
     listasTotalesPaso: [
-      { nombre: "1A", clase: "uno", claseBorde: "unoBorde" },
-      { nombre: "1B", clase: "uno", claseBorde: "unoBorde" },
-      { nombre: "1C", clase: "uno", claseBorde: "unoBorde" },
-      { nombre: "2A", clase: "dos", claseBorde: "dosBorde" },
-      { nombre: "2B", clase: "dos", claseBorde: "dosBorde" },
-      { nombre: "3A", clase: "tres", claseBorde: "tresBorde" },
-      { nombre: "4A", clase: "cuatro", claseBorde: "cuatroBorde" },
-      { nombre: "4B", clase: "cuatro", claseBorde: "cuatroBorde" },
-      { nombre: "4C", clase: "cuatro", claseBorde: "cuatroBorde" },
-      { nombre: "4D", clase: "cuatro", claseBorde: "cuatroBorde" },
+      { nombre: "1A", clase: "uno", claseBorde: "unoBorde", tipoEleccion:"paso" },
+      { nombre: "1B", clase: "uno", claseBorde: "unoBorde", tipoEleccion:"paso" },
+      { nombre: "1C", clase: "uno", claseBorde: "unoBorde", tipoEleccion:"paso" },
+      { nombre: "2A", clase: "dos", claseBorde: "dosBorde", tipoEleccion:"paso" },
+      { nombre: "2B", clase: "dos", claseBorde: "dosBorde", tipoEleccion:"paso" },
+      { nombre: "3A", clase: "tres", claseBorde: "tresBorde",tipoEleccion:"paso" },
+      { nombre: "4A", clase: "cuatro", claseBorde: "cuatroBorde",tipoEleccion:"paso" },
+      { nombre: "4B", clase: "cuatro", claseBorde: "cuatroBorde",tipoEleccion:"paso" },
+      { nombre: "4C", clase: "cuatro", claseBorde: "cuatroBorde",tipoEleccion:"paso" },
+      { nombre: "4D", clase: "cuatro", claseBorde: "cuatroBorde",tipoEleccion:"paso" },
+      { nombre: "1", clase: "uno", claseBorde: "unoBorde", tipoEleccion:"general" },
+      { nombre: "2", clase: "dos", claseBorde: "dosBorde", tipoEleccion:"general" },
+      { nombre: "3", clase: "tres", claseBorde: "tresBorde", tipoEleccion:"general" },
+      { nombre: "4", clase: "cuatro", claseBorde: "cuatroBorde", tipoEleccion:"general" }
     ],
-    listasTotales: ["1", "2", "3", "4", "5"],
+    listasTotales: [
+      { nombre: "1", clase: "uno", claseBorde: "unoBorde" },
+      { nombre: "2", clase: "dos", claseBorde: "dosBorde" },
+      { nombre: "3", clase: "tres", claseBorde: "tresBorde" },
+      { nombre: "4", clase: "cuatro", claseBorde: "cuatroBorde" },
+    ],
   },
   methods: {
-    emitirVoto : function(event){
-      this.votoGobernador = 0
-      this.votoSenadores = 0
-      this.votoDiputados = 0
-      this.votoIntendente = 0
-      this.votoConcejales = 0
-      
+    emitirVoto: function (event) {
+      this.votoGobernador = 0;
+      this.votoSenadores = 0;
+      this.votoDiputados = 0;
+      this.votoIntendente = 0;
+      this.votoConcejales = 0;
+
       var listaCompleta = this.checkedValuesListaCompleta;
       var gobernadores = this.checkedValuesGobernador;
       var senadores = this.checkedValuesSenadores;
@@ -52,77 +68,78 @@ new Vue({
       var sizeSenadores = senadores.length;
       var sizeDiputados = diputados.length;
       var sizeIntendentes = intedentes.length;
-      var sizeConcejales = concejales.length; 
-      
+      var sizeConcejales = concejales.length;
 
-
-      if(sizeListaCompleta > 1){
+      if (sizeListaCompleta > 1) {
         this.votoGobernador = 2;
         this.votoSenadores = 2;
         this.votoDiputados = 2;
         this.votoIntendente = 2;
         this.votoConcejales = 2;
-      }else if(listaCompleta.length == 1){
-        this.votoGobernador ++;
-        this.votoSenadores ++;
-        this.votoDiputados ++;
-        this.votoIntendente ++;
-        this.votoConcejales ++;
-        if(sizeGobernador > 0)  this.votoGobernador++
-        if(sizeSenadores > 0)  this.votoSenadores++
-        if(sizeDiputados > 0)  this.votoDiputados++
-        if(sizeIntendentes > 0)  this.votoIntendente++
-        if(sizeConcejales > 0)  this.votoConcejales++
-      }else{
-        if(sizeGobernador==1){
+      } else if (listaCompleta.length == 1) {
+        this.votoGobernador++;
+        this.votoSenadores++;
+        this.votoDiputados++;
+        this.votoIntendente++;
+        this.votoConcejales++;
+        if (sizeGobernador > 0) this.votoGobernador++;
+        if (sizeSenadores > 0) this.votoSenadores++;
+        if (sizeDiputados > 0) this.votoDiputados++;
+        if (sizeIntendentes > 0) this.votoIntendente++;
+        if (sizeConcejales > 0) this.votoConcejales++;
+      } else {
+        if (sizeGobernador == 1) {
           this.votoGobernador++;
-        }else if(sizeGobernador>1){
-          this.votoGobernador=2;
+        } else if (sizeGobernador > 1) {
+          this.votoGobernador = 2;
         }
 
-        if(sizeSenadores==1){
+        if (sizeSenadores == 1) {
           this.votoSenadores++;
-        }else if(sizeSenadores>1){
-          this.votoSenadores=2;
+        } else if (sizeSenadores > 1) {
+          this.votoSenadores = 2;
         }
 
-        if(sizeDiputados==1){
+        if (sizeDiputados == 1) {
           this.votoDiputados++;
-        }else if(sizeDiputados>1){
-          this.votoDiputados=2;
+        } else if (sizeDiputados > 1) {
+          this.votoDiputados = 2;
         }
 
-        if(sizeIntendentes==1){
+        if (sizeIntendentes == 1) {
           this.votoIntendente++;
-        }else if(sizeIntendentes>1){
-          this.votoIntendente=2;
+        } else if (sizeIntendentes > 1) {
+          this.votoIntendente = 2;
         }
 
-        if(sizeConcejales==1){
+        if (sizeConcejales == 1) {
           this.votoConcejales++;
-        }else if(sizeConcejales>1){
-          this.votoConcejales=2;
+        } else if (sizeConcejales > 1) {
+          this.votoConcejales = 2;
         }
       }
-
 
       this.showModal = true;
     },
 
-    limpiar : function(event){
-      this.checkedValuesListaCompleta = '';
-      this.checkedValuesGobernador = '';
-      this.checkedValuesSenadore = '';
-      this.checkedValuesDiputados = '';
-      this.checkedValuesIntendente = '';
-      this.checkedValuesConcejales = '';
+    limpiar: function (event) {
+      this.checkedValuesListaCompleta = "";
+      this.checkedValuesGobernador = "";
+      this.checkedValuesSenadore = "";
+      this.checkedValuesDiputados = "";
+      this.checkedValuesIntendente = "";
+      this.checkedValuesConcejales = "";
 
-    this.checkedValuesListaCompleta = []
-    this.checkedValuesGobernador= []
-    this.checkedValuesSenadores= []
-    this.checkedValuesDiputados= []
-    this.checkedValuesIntendente= []
-    this.checkedValuesConcejales= []
+      this.checkedValuesListaCompleta = [];
+      this.checkedValuesGobernador = [];
+      this.checkedValuesSenadores = [];
+      this.checkedValuesDiputados = [];
+      this.checkedValuesIntendente = [];
+      this.checkedValuesConcejales = [];
+    },
+    changeSelect: function(event){
+      console.log(this.tipoEleccion);
+      this.limpiar();
     }
-  }
+  },
 });
